47ad3c90ed8b97a69c820f3bb1d330ad6a111dff
Atorami <atoramiseo@mail.ru>
2023-11-09 13:41:37 +0100

    Feat: add error and empty input verifying

--- weather_aplication/main/views.py
+++ weather_aplication/main/views.py
@@ -9,16 +9,21 @@ def index(request):
         city = request.POST['location']
         api_url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid={}'
         response_data = requests.get(api_url.format(city, api_key)).json()
-        print(response_data)
-        weather_data = {
-            'temp': response_data['main']['temp'],
-            'temp_min': response_data['main']['temp_min'],
-            'temp_max': response_data['main']['temp_max'],
-            'pressure': response_data['main']['pressure'],
-            'humidity': response_data['main']['humidity'],
-            'icon_nr': response_data['weather'][0]['icon'],
-        }
-        print(weather_data)
-        return render(request, 'index.html', {'response_data': weather_data})
+
+        if response_data['cod'] == 200:
+            weather_data = {
+                'city': response_data['name'],
+                'temp': round(response_data['main']['temp']-273.15),
+                'temp_min': round(response_data['main']['temp_min']-273.15),
+                'temp_max': round(response_data['main']['temp_max']-273.15),
+                'pressure': response_data['main']['pressure'],
+                'humidity': response_data['main']['humidity'],
+                'icon_nr': response_data['weather'][0]['icon'],
+            }
+
+            return render(request, 'index.html', {'response_data': weather_data})
+        else:
+            err_msg = response_data['message'].capitalize()
+            return render(request, 'index.html', {'error': err_msg})
     else:
         return render(request, 'index.html')
