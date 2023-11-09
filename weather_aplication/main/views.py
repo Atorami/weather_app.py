59be8e0eb7f99d1fe900d7466a651788484d7fd0
Atorami <atoramiseo@mail.ru>
2023-11-09 12:12:25 +0100

    Fix : response data and icon display

--- weather_aplication/main/views.py
+++ weather_aplication/main/views.py
@@ -11,7 +11,12 @@ def index(request):
         response_data = requests.get(api_url.format(city, api_key)).json()
         print(response_data)
         weather_data = {
-            'temp': response_data['main']['temp']
+            'temp': response_data['main']['temp'],
+            'temp_min': response_data['main']['temp_min'],
+            'temp_max': response_data['main']['temp_max'],
+            'pressure': response_data['main']['pressure'],
+            'humidity': response_data['main']['humidity'],
+            'icon_nr': response_data['weather'][0]['icon'],
         }
         print(weather_data)
         return render(request, 'index.html', {'response_data': weather_data})
